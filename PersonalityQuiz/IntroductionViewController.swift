19aaedfe54b094adc84b5ddbe62c7544b59bbb2f
Unknown <kosyak_@mail.ru>
2017-09-20 15:19:50 +0300

    Created unwind segue to the IntroductionVC

--- PersonalityQuiz/IntroductionViewController.swift
+++ PersonalityQuiz/IntroductionViewController.swift
@@ -20,6 +20,10 @@ class IntroductionViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func unwindToQuizIntroduction (segue: UIStoryboardSegue) {
+        
+    }
+    
 
 }
 
