23c9579cbc5e6d2cf8c65e6c97c7d1f1545164de
Unknown <kosyak_@mail.ru>
2017-09-20 15:27:15 +0300

    Added animal description

--- PersonalityQuiz/QuestionData.swift
+++ PersonalityQuiz/QuestionData.swift
@@ -30,13 +30,13 @@ enum AnimalType: Character {
     var definition: String {
         switch self {
         case .dog:
-            return "You are and blah blah... dog"
+            return "You are incredibly outgoing. You surround yourself with the people you love and enjoy activities with your friends."
         case .cat:
-            return "You are and blah blah... cat"
+            return "Mischievous, yes mild-tempered, you enjoy doing things on your own terms."
         case .rabbit:
-            return "You are and blah blah... rabbit"
+            return "You love everything that's soft. You are healthy and full of energy."
         case .turtle:
-            return "You are and blah blah... turtle"
+            return "You are wise beyond your years, and you focus on the details. Slow and steady wins the race."
         }
     }
     
